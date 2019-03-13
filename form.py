b8b4153f5569eb99b42b217a28b0176a6f163ee4
carlsfg <46880927+carlsfg@users.noreply.github.com>
2019-03-12 23:28:50 -0600

    Add python

---
+++ form.py
@@ -0,0 +1,10 @@
+#@name EquipoGFA1
+#@date 11/03
+#@version1.0
+print "Ingresa tu nombre:"
+name = input()
+print "Ingresa tu edad:"
+age = input()
+print "Bienvenido,", name
+print "Tu edad es", age
+print "Eres de", location
