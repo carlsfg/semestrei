2a98fe35f6d3c2e7d878f05a3e531d521623532c
Carlos Flores <carlsfg@icloud.com>
2019-03-13 11:27:26 -0600

    clase 5

---
+++ add.py
@@ -0,0 +1,4 @@
+x = input("Enter first number\n") 
+y = input("Enter second number\n")
+z = input("Enter third number\n") 
+print "The total is", x+y+z
--- clase2.py
+++ clase2.py
@@ -3,4 +3,4 @@ print "Hola",name
 age = int(input("Enter your age\n"))
 nextyear = age + 1
 print "Next year you'll be", nextyear
-print "Ya sientese senora"
+print "Ya sientese seora"
