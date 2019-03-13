ddbf65c937b229d88968c1b04290ea1761a42293
Carlos Flores <carlsfg@icloud.com>
2019-03-12 23:54:25 -0600

    2

--- inConverter.py
+++ inConverter.py
@@ -1,3 +1,5 @@
-print "Ingresa la cantidad en centrimetros"
-x = input()
-print x / 2.54, "pulgadas"
+#Este es un convertidor de pulgadas 
+print "Ingresa la cantidad que deseas convertir a centrimetros"
+x = input("Ingresa un numero entero: ")
+y = x // 2.54
+print int(y), "centimetros"
