ab8a7e98938574f16ada574ef7e5c35cebb13f1f
Carlos Flores <carlsfg@icloud.com>
2019-03-13 11:29:46 -0600

    Create money.py

---
+++ money.py
@@ -0,0 +1,11 @@
+x = input("Ingresa la cantidad:")
+renta = x*0.25
+servicios = x*0.15
+comida = x*0.3
+tareas = x*0.2
+diversion = x*0.1
+print "Renta", "$", renta
+print "Servicios","$", servicios
+print "Comida","$", comida
+print "Tareas","$", tareas
+print "Diversion","$", diversion
