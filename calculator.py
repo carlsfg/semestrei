9427f53c95e5c1dd1a2780a9c61d5336a4373e77
Carlos Flores <carlsfg@icloud.com>
2019-03-13 18:53:04 -0600

    commit5

--- calculator.py
+++ calculator.py
@@ -50,48 +50,66 @@ def salesTax():
 
 #This function is to calculate the income tax
 def incomeTax():
-    x = input ("Type anual income: $")
-    y = input ("Type anual outcome: $")
+    x = float (input("Type anual income: $"))
+    y = float (input("Type anual outcome: $"))
+    z = float (input("Type the tax rate in: %"))
     if y > x:
         print "You don't have to pay taxes."
-    else:
-        (x-y)*.3
+    if x > y:
+        taxToPay=(x-y)*(z/100)
+        print "Your tax pay is: $", taxToPay
+        
+#Options
+def main():
+    print ("Please, choose from one of the following: ")
+    print (30 * '-')
+    print ("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n5. Power to\n6. Convert Farenheit to Celcuis\n7. Calculate the sale tax\n8. Calculate the income tax\n9. Exit")
+    selection = input("Insert option:")
+    
+    if selection == 1:
+        add()
+
+    if selection == 2:
+        subtract()
+
+    if selection == 3:
+        multiply()
+
+    if selection == 4:
+        divide()
+
+    if selection == 5:
+        power()
+
+    if selection == 6:
+        convert()
+
+    if selection == 7:
+        salesTax()
+
+    if selection == 8:
+        incomeTax()
+
+    if selection == 9:
+        print ("Goodbye")
+        exit()
 
 #Menu
 print (30 * '-')
-print ("Welcome to the Python Calculator")
+print ("W E L C O M E  T O  T H E  P Y T H O N  C A L C U L A T O R")
 print (30 * '-')
+main()
 
-#Options
-print ("Please, choose from one of the following: ")
-print ("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n5. Power to\n6. Convert Farenheit to Celcuis\n7. Calculate the sale tax\n8. Calculate the income tax\n9. Exit")
-selection = input("Insert option:")
-    
-if selection == 1:
-    add()
-
-if selection == 2:
-    subtract()
-
-if selection == 3:
-    multiply()
-
-if selection == 4:
-    divide()
-
-if selection == 5:
-    power()
-
-if selection == 6:
-    convert()
-
-if selection == 7:
-    salesTax()
-
-if selection == 8:
-    incomeTax()
-
-if selection == 9:
-    print ("Goodbye")
+#Return
+start = raw_input("Do you want to run the calculator again? (y/n): ")
+if start not in ('y', 'n'):
+    print("Invalid input. Sorry")
     exit()
-
+elif start == 'y':
+    main()
+elif start == 'n':
+    print("Goodbye")
+    exit()
+else:
+    print("Invalid input. Sorry")
+    exit()
