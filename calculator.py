352badd315cae9c6e4e87fbcc64c0669b191dd01
Carlos Flores <carlsfg@icloud.com>
2019-03-13 19:29:06 -0600

    final

--- calculator.py
+++ calculator.py
@@ -7,38 +7,38 @@
 
 #This function is for adding two numbers
 def add():
-    x = input ("Type number 1: ")
-    y = input ("Type number 2: ")
+    x = float (input ("Type number 1: "))
+    y = float (input ("Type number 2: "))
     print ("The result is:"), x + y
 
 #This function is for substracting two numbers
 def subtract():
-    x = input ("Type number 1: ")
-    y = input ("Type number 2: ")
+    x = float (input ("Type number 1: "))
+    y = float (input ("Type number 2: "))
     print ("The result is:"), x - y
 
 #This function is for multiplying two numbers
 def multiply():
-    x = input ("Type number 1: ")
-    y = input ("Type number 2: ")
+    x = float (input ("Type number 1: "))
+    y = float (input ("Type number 2: "))
     print ("The result is:"), x * y
 
 #This function is for dividing two numbers
 def divide():
-    x = input ("Type number 1: ")
-    y = input ("Type number 2: ")
+    x = float (input ("Type number 1: "))
+    y = float (input ("Type number 2: "))
     print ("The result is:"), x / y
 
 #This function is to elevate a number to the power of a number
 def power():
-    x = input ("Type base number: ")
-    y = input ("Type power number: ")
+    x = float (input ("Type base number: "))
+    y = float (input ("Type power number: "))
     print ("The result is:"), x ** y
 
 #This function is to convert Farenheit to Celsius
 def convert():
-    x = input ("Type temperature in Farentheit: ")
-    print (x - 32) * 5 / 9
+    x = int (input ("Type temperature in Farentheit: "))
+    print ("The temperature in Celsius is :"), (x - 32) * 5 / 9
 
 #This function is to calculate the sales tax
 def salesTax():
@@ -58,58 +58,65 @@ def incomeTax():
     if x > y:
         taxToPay=(x-y)*(z/100)
         print "Your tax pay is: $", taxToPay
-        
+
 #Options
 def main():
     print ("Please, choose from one of the following: ")
-    print (30 * '-')
+    print (40 * '-')
     print ("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n5. Power to\n6. Convert Farenheit to Celcuis\n7. Calculate the sale tax\n8. Calculate the income tax\n9. Exit")
+    print ("\n")
     selection = input("Insert option:")
     
+    #Conditionals
     if selection == 1:
         add()
 
-    if selection == 2:
+    elif selection == 2:
         subtract()
 
-    if selection == 3:
+    elif selection == 3:
         multiply()
 
-    if selection == 4:
+    elif selection == 4:
         divide()
 
-    if selection == 5:
+    elif selection == 5:
         power()
 
-    if selection == 6:
+    elif selection == 6:
         convert()
 
-    if selection == 7:
+    elif selection == 7:
         salesTax()
 
-    if selection == 8:
+    elif selection == 8:
         incomeTax()
 
-    if selection == 9:
+    elif selection == 9:
         print ("Goodbye")
+        exit()
+    
+    else:
+        print ("Please type a valid option")
+
+    #This section is to return to use if user wants to   
+    
+    start = raw_input("Do you want to run the calculator again? (y/n): ")
+    if start not in ('y', 'n'):
+        print("Invalid input. Sorry")
+        exit()
+    elif start == 'y':
+        main()
+    elif start == 'n':
+        print("Goodbye")
+        exit()
+    else:
+        print("Invalid input. Sorry")
         exit()
 
 #Menu
-print (30 * '-')
+print (50 * '-')
 print ("W E L C O M E  T O  T H E  P Y T H O N  C A L C U L A T O R")
-print (30 * '-')
+print (50 * '-')
 main()
 
-#Return
-start = raw_input("Do you want to run the calculator again? (y/n): ")
-if start not in ('y', 'n'):
-    print("Invalid input. Sorry")
-    exit()
-elif start == 'y':
-    main()
-elif start == 'n':
-    print("Goodbye")
-    exit()
-else:
-    print("Invalid input. Sorry")
-    exit()
