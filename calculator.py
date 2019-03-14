5951fae174cfe2bbb7a93618b1fee5384eb0293f
Carlos Flores <carlsfg@icloud.com>
2019-03-13 18:25:41 -0600

    commit2

---
+++ calculator.py
@@ -0,0 +1,86 @@
+#@Author: Carlos Flores
+#@Matricula: A01206352
+#@Version: 1.0
+#@Date: 13/03/19
+
+#This Python script is a calculator that can add, subtract, multiply, divide, convert celcius to farenheit, calculate the income tax and the sales tax.
+
+#This function is for adding two numbers
+def add():
+    x = input ("Type number 1: ")
+    y = input ("Type number 2: ")
+    print ("The result is:"), x + y
+
+#This function is for substracting two numbers
+def subtract():
+    x = input ("Type number 1: ")
+    y = input ("Type number 2: ")
+    print ("The result is:"), x - y
+
+#This function is for multiplying two numbers
+def multiply():
+    x = input ("Type number 1: ")
+    y = input ("Type number 2: ")
+    print ("The result is:"), x * y
+
+#This function is for dividing two numbers
+def divide():
+    x = input ("Type number 1: ")
+    y = input ("Type number 2: ")
+    print ("The result is:"), x / y
+
+#This function is to elevate a number to the power of a number
+def power():
+    x = input ("Type base number: ")
+    y = input ("Type power number 2: ")
+    print ("The result is:"), x ^ y
+
+#This function is to convert Farenheit to Celsius
+def convert():
+    x = input ("Type temperature in Farentheit: ")
+    print x - 32 * 5 / 9
+
+#This function is to calculate the sales tax
+def salesTax():
+    x = input ("Type subtotal: ")
+    y = input ("Type tax in %: ")
+    print x * (1+(y/100)
+
+    
+#Menu
+print (30 * '-')
+print ("Welcome to the Python Calculator")
+print (30 * '-')
+
+#Options
+print ("Please, choose from one of the following: ")
+print ("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n5. Power to\n6. Convert Farenheit to Celcuis\n7. Calculate the sale tax\n8. Calculate the income tax\n9. Exit")
+selection = input("Insert option:")
+    
+if selection == 1:
+    add()
+
+if selection == 2:
+    subtract()
+
+if selection == 3:
+    multiply()
+
+if selection == 4:
+    divide()
+
+if selection == 5:
+    power()
+
+if selection == 6:
+    convert()
+
+if selection == 7:
+    salesTax()
+
+if selection == 8:
+    incomeTax()
+
+if selection == 9:
+    exit()
+
